fe3d82c9f1a64b28e5d797e51a21a679e0d12a5e
aokitashipro <webmocha2@gmail.com>
2022-12-26 10:41:05 +0900

    add sec6

--- section6/my-app/src/App.js
+++ section6/my-app/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import TestComponent from './components/TestComponent';
+import GetItemComponent from './components/GetItemComponent';
+import StyledComponent from './components/StyledComponent';
+import StyledUseStateProps from './components/StyledUseStateProps';
+import EmotionComponent from './components/EmotionComponent';
 
 function App() {
   return (
     <div className="App">
+      <TestComponent />
+      <GetItemComponent />
+      <StyledComponent />
+      <StyledUseStateProps />
+      <EmotionComponent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
